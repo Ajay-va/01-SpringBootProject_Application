610f76822b42ddafea8a79a8a15fb9e1503c0ffd
Ajay-va <javvajiajayvarma222@gmail.com>
2021-07-25 14:37:53 +0530

    address added to feature-302

---
+++ src/main/java/com/example/demo/Address.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Address {
+
+	
+	private int pincode;
+	private String street;
+	private String village;
+	private String mandal;
+	private String distict;
+	
+}
